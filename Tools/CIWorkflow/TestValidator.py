6a8637595a2bdf5486a333cecb1e91418a9acadd
Oxmose <alexy.torresa@gmail.com>
2023-05-13 20:08:17 +0200

    Minor CI fixes

--- Tools/CIWorkflow/TestValidator.py
+++ Tools/CIWorkflow/TestValidator.py
@@ -148,6 +148,9 @@ def UpdateTestFile(filename, testGroup, testName):
 
 
 if __name__ == "__main__":
+    print(COLORS.OKBLUE + COLORS.BOLD + "#==============================================================================#" + COLORS.ENDC)
+    print(COLORS.OKBLUE + COLORS.BOLD + "| UTK UNIT TEST FRAMEWORK                                                      |" + COLORS.ENDC)
+    print(COLORS.OKBLUE + COLORS.BOLD + "#==============================================================================#"  + COLORS.ENDC)
     if len(sys.argv) != 5:
         print("Usage: {} target test_group_file test_list test_file_output".format(sys.argv[0]))
         exit(1)

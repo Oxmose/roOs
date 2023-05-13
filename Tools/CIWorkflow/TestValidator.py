cbc1f8aa4921f592cd787e0d1e615928942cdaf6
Oxmose <alexy.torresa@gmail.com>
2023-05-13 20:46:23 +0200

    Minor CI fixes

--- Tools/CIWorkflow/TestValidator.py
+++ Tools/CIWorkflow/TestValidator.py
@@ -1,4 +1,37 @@
+import json
 import sys
+import os
+import re
+
+class COLORS:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+TYPE_STR = [
+    "BYTE",
+    "UBYTE",
+    "HALF",
+    "UHALF",
+    "WORD",
+    "UWORD",
+    "DWORD",
+    "UDWORD",
+    "FLOAT",
+    "DOUBLE",
+    "RCODE"
+]
+
+TARGET_LIST = [
+    "x86_64",
+    "x86_i386"
+]
 
 if __name__ == "__main__":
     print(COLORS.OKBLUE + COLORS.BOLD + "#==============================================================================#" + COLORS.ENDC)

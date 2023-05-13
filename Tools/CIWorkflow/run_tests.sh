8392bb6a6ac6cbc4c783b1c501e7ff8c89e93bfc
Oxmose <alexy.torresa@gmail.com>
2023-05-13 17:53:37 +0200

    Minor CI fixes

--- Tools/CIWorkflow/run_tests.sh
+++ Tools/CIWorkflow/run_tests.sh
@@ -3,5 +3,5 @@
 pwd
 cd Source
 
-python3 ../Tools/CIWorkflow/TestValidator.py x86_64 Kernel/TestFramework/includes/test_groups.json Kernel/TestFramework/includes/test_list.h ../test_file_output.txt
-python3 ../Tools/CIWorkflow/TestValidator.py x86_i386 Kernel/TestFramework/includes/test_groups.json Kernel/TestFramework/includes/test_list.h ../test_file_output.txt
+python ../Tools/CIWorkflow/TestValidator.py x86_64 Kernel/TestFramework/includes/test_groups.json Kernel/TestFramework/includes/test_list.h ../test_file_output.txt
+python ../Tools/CIWorkflow/TestValidator.py x86_i386 Kernel/TestFramework/includes/test_groups.json Kernel/TestFramework/includes/test_list.h ../test_file_output.txt
